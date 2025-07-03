07000c57078f76a45a7196b9a60c7201f6ad698a
Arun R <153985734+itsme-arun@users.noreply.github.com>
2025-07-03 13:33:06 +0530

    Update Main.java

--- ConfiguringSpringApp/src/main/java/org/example/Main.java
+++ ConfiguringSpringApp/src/main/java/org/example/Main.java
@@ -7,13 +7,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        // Load the Spring XML config
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Get the bean from Spring container
         Employee emp = (Employee) context.getBean("employee");
 
-        // Print the bean data
         System.out.println(emp);
     }
 }
