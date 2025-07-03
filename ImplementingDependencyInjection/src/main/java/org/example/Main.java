cc7050e20066dcf0042dafb757e6729ab67c4684
Arun R <153985734+itsme-arun@users.noreply.github.com>
2025-07-03 20:38:19 +0530

    ImplementingDependencyInjection

---
+++ ImplementingDependencyInjection/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.model.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Employee emp = (Employee) context.getBean("employee");
+
+        System.out.println(emp);
+    }
+}
